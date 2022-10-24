ea3b2caee98354660f8b8954491b3d2550addf39
Deepankar <dip2leo@gmail.com>
2022-10-24 00:51:30 -0400

    finish profile update

--- Users/models.py
+++ Users/models.py
@@ -1,7 +1,9 @@
 from distutils.command.upload import upload
 from email.policy import default
+from turtle import width
 from django.db import models
 from django.contrib.auth.models import User #to get the users 
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):   #1-1 relationship with existing user model 
@@ -10,3 +12,13 @@ class Profile(models.Model):   #1-1 relationship with existing user model
 
     def __str__(self) -> str:
         return f"{self.user.username} Profile" #pritns current logged in user name
+    
+    # to resize image
+    def save(self): #gets run after our model is saved. Overrriding it
+        super().save() #grab the image and resize it
+        
+        img = Image.open(self.image.path) 
+        if (img.height > 300 or img.width > 300):
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
